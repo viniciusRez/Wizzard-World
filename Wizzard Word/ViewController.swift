4f1182ba8478e4a9415c1fd52423772b492789d7
Vinicius Rezende <vinicius.rezende@rethink.dev>
2023-01-10 09:07:00 -0300

    Initial Commit

---
+++ Wizzard Word/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wizzard Word
+//
+//  Created by Vinicius Rezende on 10/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
